005b6f5d65a536f0e06b086a3ab81061c6db6c07
Victor <maragiogliov@protonmail.com>
2023-03-27 12:55:34 -0400

    Car Progress

--- src/content/CarPage/Car_FootprintsButton.js
+++ src/content/CarPage/Car_FootprintsButton.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
-
 import { 
-  setFoodDrinksFootprint, 
-  setShowFoodDrinksResult, 
+  setCarResult,
+  setCarFootprint,
+  setShowCarResult 
 
-  setPharmaceuticalsFootprint, 
-  setShowPharmaceuticalsResult,
-
-  setTextilesFootprint, 
-  setShowTextilesResult,
-
-  setPaperFootprint, 
-  setShowPaperResult,
-
-  setElectronicsFootprint, 
-  setShowElectronicsResult,
-
-  setFurnitureFootprint,
-  setShowFurnitureResult,
-
-  setHotelsFootprint, 
-  setShowHotelsResult
- } from "../../redux/secondary";
+ } from "../../redux/car";
 
 import countryFactors from '../../countryFactors'
 
@@ -32,169 +15,41 @@ import { Calculator } from '@carbon/react/icons';
 
 const SecondaryFootprint = () => {
   const { 
-    numPeople,
-    selectedCountrySecondary, 
+    selectCountryCar, 
+    carMileage,
+    carUnit,
+    carType,
+    carSize
 
-    foodDrinks,
-    rangeFoodDrinks,
-
-    pharmaceuticals, 
-    rangePharmaceuticals,
-
-    textiles, 
-    rangeTextiles, 
-
-    paper, 
-    rangePaper,
-
-    electronics, 
-    rangeElectronics,
-
-    furniture, 
-    rangeFurniture,
-
-    hotels, 
-    rangeHotels
-  
   } = useSelector(
-    (state) => state.secondary
+    (state) => state.car
   );
   const dispatch = useDispatch();
 
-  const showFootprintResultsFoodDrinks = () => {
-    if (foodDrinks > 0) {
-      dispatch(setShowFoodDrinksResult(true));
-    }
-  };
-  const showFootprintResultsPharmaceuticals = () => {
-    if (pharmaceuticals > 0){
-      dispatch( setShowPharmaceuticalsResult(true));
-    }
-  };
-  const showFootprintResultsTextiles = () => {
-    if (textiles > 0){
-      dispatch( setShowTextilesResult(true));
-    }
-  };
-  const showFootprintResultsPaper = () => {
-    if (paper > 0){
-      dispatch( setShowPaperResult(true));
-    }
-  };
-  const showFootprintResultsElectronics = () => {
-    if (electronics > 0){
-      dispatch( setShowElectronicsResult(true));
-    }
-  };
-  const showFootprintResultsFurniture = () => {
-    if (furniture > 0){
-      dispatch( setShowFurnitureResult(true));
-    }
-  };
-  const showFootprintResultsHotels = () => {
-    if(hotels > 0){
-      dispatch( setShowHotelsResult(true));
+  const showFootprintResultsCar = () => {
+    if (carMileage > 0) {
+      dispatch(setShowCarResult (true));
     }
   };
 
-  const calculateAllHouseFootprint = () =>{
-
-    const calculateFoodDrinksFootprint = () => {
-      if (
-        isNaN(parseFloat(foodDrinks))
-      ) {
-        dispatch(setFoodDrinksFootprint("Please enter a valid number."));
+  const calculateCarAndMotorbikeFootprint = () => {
+    const calculateCarFootprint = () => {
+      if (isNaN(parseFloat(carMileage))) {
+        dispatch(setCarFootprint("Please enter a valid number."));
       } else {
-        let total = (parseFloat(foodDrinks) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].foodDrinksRange[rangeFoodDrinks].foodDrinks_factor) * (0.001);
-        dispatch(setFoodDrinksFootprint(total.toFixed(2)));
+        let total =
+          parseFloat(carMileage)
+        dispatch(setCarFootprint(total.toFixed(2)));
+        showFootprintResultsCar();
       }
-      showFootprintResultsFoodDrinks()
     };
-
-
-    const calculatePharmaceuticalsFootprint = () => {
-      if (
-        isNaN(parseFloat(pharmaceuticals))
-      ) {
-        dispatch(setPharmaceuticalsFootprint("Please enter a valid number."));
-      } else {
-        let total = (parseFloat(pharmaceuticals) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].pharmaceuticalsRange[rangePharmaceuticals].pharmaceuticals_factor) * (0.001);
-        dispatch(setPharmaceuticalsFootprint(total.toFixed(2)));
-      }
-      showFootprintResultsPharmaceuticals()
-    };
-
-
-    const calculateTextilesFootprint = () => {
-        if (
-          isNaN(parseFloat(textiles))
-        ) {
-          dispatch(setTextilesFootprint("Please enter a valid number."));
-        } else {
-          let total = (parseFloat(textiles) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].textilesRange[rangeTextiles].textiles_factor) * (0.001);
-          dispatch(setTextilesFootprint(total.toFixed(2)));
-        }
-        showFootprintResultsTextiles()
-    };
-    const calculatePaperFootprint = () => {
-        if (
-            isNaN(parseFloat(paper))
-        ) {
-            dispatch(setPaperFootprint("Please enter a valid number."));
-        } else {
-            let total = (parseFloat(paper) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].paperRange[rangePaper].paper_factor) * (0.001);
-            dispatch(setPaperFootprint(total.toFixed(2)));
-        }
-        showFootprintResultsPaper()
-
-    };
-    const calculateElectronicsFootprint = () => {
-        if (
-        isNaN(parseFloat(electronics))
-        ) {
-        dispatch(setElectronicsFootprint("Please enter a valid number."));
-        } else {
-        let total = (parseFloat(electronics) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].electronicsRange[rangeElectronics].electronics_factor) * (0.001);
-        dispatch(setElectronicsFootprint(total.toFixed(2)));
-        }
-        showFootprintResultsElectronics()
-
-    };
-    const calculateFurnitureFootprint = () => {
-    if (
-      isNaN(parseFloat(furniture))
-    ) {
-      dispatch(setFurnitureFootprint("Please enter a valid number."));
-    } else {
-      let total = (parseFloat(furniture) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].furnitureRange[rangeFurniture].furniture_factor) * (0.001);
-      dispatch(setFurnitureFootprint(total.toFixed(2)));
-    }
-    showFootprintResultsFurniture()
-
-    };
-    const calculateHotelsFootprint = () => {
-    if (
-      isNaN(parseFloat(hotels))
-    ) {
-      dispatch(setHotelsFootprint("Please enter a valid number."));
-    } else {
-      let total = (parseFloat(hotels) / parseFloat(numPeople)) * (countryFactors[selectedCountrySecondary].hotelsRange[rangeHotels].hotels_factor) * (0.001);
-      dispatch(setHotelsFootprint(total.toFixed(2)));
-    }
-    showFootprintResultsHotels()
-
-    };
-    calculateFoodDrinksFootprint();
-    calculatePharmaceuticalsFootprint();
-    calculateTextilesFootprint();
-    calculatePaperFootprint();
-    calculateElectronicsFootprint();
-    calculateFurnitureFootprint();
-    calculateHotelsFootprint();
-}
+  
+    calculateCarFootprint();
+  };
+  
   return <>  
     <Button 
-        onClick={calculateAllHouseFootprint} 
+        onClick={calculateCarAndMotorbikeFootprint} 
         className='car-calculate-footprint-button'
         > Calculate Car & Motorbike Footprint
        
--- src/content/CarPage/Car_TotalResults.js
+++ src/content/CarPage/Car_TotalResults.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {  useSelector } from "react-redux";
 
 const BusRailTotalResult = () => {
-  const { foodDrinksFootprint, pharmaceuticalsFootprint,textilesFootprint, paperFootprint,electronicsFootprint, furnitureFootprint, hotelsFootprint } = useSelector(
-    (state) => state.secondary
+  const { carFootprint} = useSelector(
+    (state) => state.car
   );
-  const totalSecondaryFootprint = (parseFloat(foodDrinksFootprint) + parseFloat(pharmaceuticalsFootprint)+ parseFloat(textilesFootprint)+ parseFloat(paperFootprint)+ parseFloat(electronicsFootprint)+ parseFloat(furnitureFootprint)+ parseFloat(hotelsFootprint) ||  0);
+  const totalCarFootprint = (parseFloat(carFootprint)||  0);
 
   return <>
-          <div className='car-results'>Calculated Car & Motorbike Footprint = <span style={{ marginLeft: '5px', marginRight: '5px' }}>{(totalSecondaryFootprint.toFixed(2))}</span> metric tons of CO2e</div>
+          <div className='car-results'>Calculated Car & Motorbike Footprint = <span style={{ marginLeft: '5px', marginRight: '5px' }}>{(totalCarFootprint.toFixed(2))}</span> metric tons of CO2e</div>
   </>;
 };
 
