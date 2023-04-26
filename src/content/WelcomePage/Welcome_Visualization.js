eb3734f0b76daab026f5b350a16bd6b1cb2b32fa
Victor <maragiogliov@protonmail.com>
2023-04-27 00:10:27 +0200

    is this working again?

--- src/content/WelcomePage/Welcome_Visualization.js
+++ src/content/WelcomePage/Welcome_Visualization.js
@@ -3,7 +3,14 @@ import { DonutChart } from '@carbon/charts-react';
 import topCarbonEmittersPerCapita from '../../countryAverage';
 
 const WorldAverage = () => {
-
+  const theme = {
+    background: '#FFFFFF',
+    colors: {
+      interactive01: '#003B5C',
+      interactive02: '#054ADA',
+      text01: '#003B5C',
+    },
+  };
   // Define the data for the donut chart
   const data = Object.entries(topCarbonEmittersPerCapita).map(([country, emissions]) => ({
     group: country,
@@ -12,25 +19,26 @@ const WorldAverage = () => {
 
   // Define the options for the donut chart
   const options = {
-    "title": "",
+    "title": "Carbon Emissions per Capita by Country",
     "resizable": true,
     "legend": {
       "alignment": "center"
     },
     "donut": {
       "center": {
-        "label": "Kg of CO2e"
+        "label": "Tonnes of CO2e per year"
       },
       "precision": 2,
       "alignment": "center"
     },
-    "height": "360px",
+    "height": "600px",
+    
   }
 
   return (
     <>
       <div>
-        <DonutChart data={data} options={options} />
+        <DonutChart data={data} options={options}  />
       </div>
     </>
   );
