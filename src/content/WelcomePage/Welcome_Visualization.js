4263aeb606b6e8640784c9a9feae792bbae8af4a
Victor <maragiogliov@protonmail.com>
2023-04-27 12:27:16 +0200

    go

--- src/content/WelcomePage/Welcome_Visualization.js
+++ src/content/WelcomePage/Welcome_Visualization.js
@@ -1,45 +1,30 @@
 import React from 'react';
-import { DonutChart } from '@carbon/charts-react';
+import { TreemapChart } from '@carbon/charts-react';
+
 import topCarbonEmittersPerCapita from '../../countryAverage';
 
 const WorldAverage = () => {
-  const theme = {
-    background: '#FFFFFF',
-    colors: {
-      interactive01: '#003B5C',
-      interactive02: '#054ADA',
-      text01: '#003B5C',
-    },
-  };
-  // Define the data for the donut chart
-  const data = Object.entries(topCarbonEmittersPerCapita).map(([country, emissions]) => ({
-    group: country,
-    value: emissions,
+
+  // Define the data for the treemap chart
+  const treemapChartData = Object.entries(topCarbonEmittersPerCapita).map(([country, emissions]) => ({
+    name: country,
+    children: [
+      {
+        name: 'Emissions',
+        value: emissions,
+      },
+    ],
   }));
 
-  // Define the options for the donut chart
-  const options = {
-    "title": "Carbon Emissions per Capita by Country",
-    "resizable": true,
-    "legend": {
-      "alignment": "center"
-    },
-    "donut": {
-      "center": {
-        "label": "Tonnes of CO2e per year"
-      },
-      "precision": 2,
-      "alignment": "center"
-    },
-    "height": "600px",
-    
-  }
+  const treemapOptions = {
+    title: 'Countries with the top 30 carbon emissions per capita',
+    height: '400px',
+  };
 
   return (
     <>
-      <div>
-        <DonutChart data={data} options={options}  />
-      </div>
+   
+      <TreemapChart data={treemapChartData} options={treemapOptions} />
     </>
   );
 };
